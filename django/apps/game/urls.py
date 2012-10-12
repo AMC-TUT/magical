9826a4f65dfe180d3d3b1087976f0851dd75e4eb
Mikko Koskela <mikko.koskela@gmail.com>
2012-10-12 15:30:18 +0300

    Added invent page to open crystal ball.

--- django/apps/game/urls.py
+++ django/apps/game/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     url(r'^details/(?P<gameslug>[\w.@+-]+)$', 'apps.game.views.game_details', name="game_details"),
     url(r'^create/$', 'apps.game.views.create_game', name="create_game"),
     url(r'^learn/$', direct_to_template, {'template': 'apps/game/learn.html'}, name="learn"),
+    url(r'^invent/$', direct_to_template, {'template': 'apps/game/invent.html'}, name="invent"),
 
     url(r'^login$', 'django.contrib.auth.views.login', {'template_name': 'apps/game/login.html'}),
     url(r'^logout/$', 'apps.game.views.logout_view'),
